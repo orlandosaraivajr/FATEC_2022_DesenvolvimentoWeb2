6b3c9fbe3c1018fe357f9bd60598711bc288e10e
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-03-15 20:19:23 -0300

    Desafio - Aula 5

---
+++ aula5/desafio/adivinhe_senha.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Passagem de Parâmetros via GET</title>
+</head>
+<body>
+    <form action="gabarito_senha.php" method="post">
+        <label for="senha">Digite a senha:</label>
+        <input type="text" id="senha" name="senha"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ aula5/desafio/gabarito_senha.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adivinhando a senha</title>
+</head>
+<body>
+<?php
+    print_r($_POST);
+?>
+</body>
+</html>
